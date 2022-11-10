6920d37cee63b7d94d684fc43ee25513a02b6839
as_mih <michelledearaujo2@gmail.com>
2022-11-10 14:23:46 -0300

    correção botão back home

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -42,7 +42,7 @@ export const signUpFunction = () => {
 
   const btnBackHome = containerSignUp.querySelector('#backAtHome');
   btnBackHome.addEventListener('click', () => {
-    window.location.assign('/');
+    window.location.assign('https://asmih.github.io/SAP008-social-network/#home');
   });
 
   return containerSignUp;
